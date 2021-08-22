c29c6a7bb77fa679215a10b21901d63dd23751fe
Saurabh Kansal <saurabh1999kansal@gmail.com>
2021-08-22 20:46:35 +0530

    second commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCkbplbZcrlRkmx0SC8zTu0yu7GcrdrRpE",
+    authDomain: "ipod-1b4ea.firebaseapp.com",
+    projectId: "ipod-1b4ea",
+    storageBucket: "ipod-1b4ea.appspot.com",
+    messagingSenderId: "1010637550540",
+    appId: "1:1010637550540:web:ca64b682507e6990ede369",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
